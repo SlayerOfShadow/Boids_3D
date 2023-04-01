44b4f14546276083186010a5a2e35a436622ef82
Antoine Leblond <antoine.leb@hotmail.fr>
2023-04-01 15:04:56 +0200

    Modified the shaders and the main, but still not displaying the sphere...

--- boids/main.cpp
+++ boids/main.cpp
@@ -2,6 +2,8 @@
 #include "../src-common/glimac/common.hpp"
 #include "../src-common/glimac/sphere_vertices.hpp"
 #include "glimac/common.hpp"
+#include "glm/ext/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
 struct Vertex2DColor {
     glm::vec2 position;
@@ -17,6 +19,11 @@ int main()
         "shaders/shader.vs.glsl",
         "shaders/shader.fs.glsl"
     );
+    shader.use();
+
+    GLuint uMVPMatrixLocation    = glGetUniformLocation(shader.id(), "uMVPMatrix");
+    GLuint uMVMatrixLocation     = glGetUniformLocation(shader.id(), "uMVMatrix");
+    GLuint uNormalMatrixLocation = glGetUniformLocation(shader.id(), "uNormalMatrix");
 
     GLuint vbo;
     glGenBuffers(1, &vbo);
@@ -36,32 +43,34 @@ int main()
     glBindVertexArray(vao);
 
     static constexpr GLuint VERTEX_ATTR_POSITION  = 0;
-    static constexpr GLuint VERTEX_ATTR_COLOR     = 1;
+    static constexpr GLuint VERTEX_ATTR_NORMAL    = 1;
     static constexpr GLuint VERTEX_ATTR_TEXCOORDS = 2;
     glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
-    glEnableVertexAttribArray(VERTEX_ATTR_COLOR);
+    glEnableVertexAttribArray(VERTEX_ATTR_NORMAL);
     glEnableVertexAttribArray(VERTEX_ATTR_TEXCOORDS);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glVertexAttribPointer(VERTEX_ATTR_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)offsetof(glimac::ShapeVertex, position));
-    glVertexAttribPointer(VERTEX_ATTR_COLOR, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)offsetof(glimac::ShapeVertex, normal));
+    glVertexAttribPointer(VERTEX_ATTR_NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)offsetof(glimac::ShapeVertex, normal));
     glVertexAttribPointer(VERTEX_ATTR_TEXCOORDS, 2, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)offsetof(glimac::ShapeVertex, texCoords));
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glBindVertexArray(0);
 
-    glGetUniformLocation(shader.id(), "position");
-    glGetUniformLocation(shader.id(), "normal");
-    glGetUniformLocation(shader.id(), "texCoords");
+    glEnable(GL_DEPTH_TEST);
 
     // Application loop :
     ctx.update = [&]() {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glm::mat4 ProjMatrix   = glm::perspective(glm::radians(70.f), ctx.aspect_ratio(), 0.1f, 100.0f);
+        glm::mat4 MVMatrix     = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
+        glm::mat4 NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUniformMatrix4fv(uMVPMatrixLocation, 1, GL_FALSE, glm::value_ptr(ProjMatrix * MVMatrix));
+        glUniformMatrix4fv(uMVMatrixLocation, 1, GL_FALSE, glm::value_ptr(MVMatrix));
+        glUniformMatrix4fv(uNormalMatrixLocation, 1, GL_FALSE, glm::value_ptr(NormalMatrix));
 
         glBindVertexArray(vao);
-        shader.use();
-
-        // => On utilise glDrawElements à la place de glDrawArrays
-        // Cela indique à OpenGL qu'il doit utiliser l'IBO enregistré dans le VAO
         glDrawArrays(GL_TRIANGLES, 0, vertices.size());
 
         glBindVertexArray(0);
